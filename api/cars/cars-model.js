08be4e50ea4bc870a2f7eb3f2889e89b84789236
BruvLio <lionel.mbayu@gmail.com>
2023-05-02 08:36:40 -0500

    wip

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,13 +1,25 @@
+const db = require("../../data/db-config");
+
 const getAll = async () => {
   // DO YOUR MAGIC
+
+  const result = await db("Cars");
+  return result;
 };
 
-const getById = async () => {
+const getById = async (Car_id) => {
   // DO YOUR MAGIC
+
+  const result = await db("Cars").where("Car_id", Car_id).first();
+  return result;
 };
 
-const create = async () => {
+const create = async (car) => {
   // DO YOUR MAGIC
+
+  const [Car_id] = await db("Cars").insert(car);
+  const result = await getById(Car_id);
+  return result;
 };
 
 module.exports = {
