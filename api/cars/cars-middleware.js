c62d167ed028f920a34c575a9470df50cdfe5c50
BruvLio <lionel.mbayu@gmail.com>
2023-05-02 08:48:42 -0500

    wip

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -2,6 +2,13 @@ const db = require("../../data/db-config");
 
 const checkCarId = async (req, res, next) => {
   // DO YOUR MAGIC
+  const { id } = req.params.id;
+  const car = await db("Cars").where("Car_id", id).first();
+  if (!car) {
+    next({ status: 404, message: `car with id ${id} is not found` });
+  } else {
+    next();
+  }
 };
 
 const checkCarPayload = async (req, res, next) => {
