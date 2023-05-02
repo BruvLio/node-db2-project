c6fc47a1cdd5c5203e9ec2ce160d57ef6563953f
BruvLio <lionel.mbayu@gmail.com>
2023-05-01 19:38:24 -0500

    wip

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,11 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  await knex.schema.createTable('', () => {
+    table.increments('')
+  })
 };
 
-exports.down = function (knex) {
+exports.down = function  (knex) {
   // DO YOUR MAGIC
+  await knex.schema.dropTableIfExists('')
 };
