b89fa4f9707e92460b37c8c5f8aa73f2297485c4
BruvLio <lionel.mbayu@gmail.com>
2023-05-01 22:07:53 -0500

    wip

--- data/seeds/cars.js
+++ data/seeds/cars.js
@@ -6,20 +6,23 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("Cars").truncate();
   await knex("Cars").insert([
-    { Car_id: "rowValue1" },
-    { Vin_Number: "rowValue2" },
-    { Car_Make: "rowValue3" },
-    { Car_Model: "rowValue3" },
-    { Mileage: "rowValue3" },
-    { Title: "rowValue3" },
-    { Transmission_Type: "rowValue3" },
+    {
+      Car_id: "rowValue1",
+      Vin_Number: "rowValue2",
+      Car_Make: "rowValue3",
+      Car_Model: "rowValue3",
+      Mileage: "rowValue3",
+      Title: "rowValue3",
+      ransmission_Type: "rowValue3",
+    },
+    {
+      Car_id: "rowValue1",
+      Vin_Number: "rowValue2",
+      Car_Make: "rowValue3",
+      Car_Model: "rowValue3",
+      Mileage: "rowValue3",
+      Title: "rowValue3",
+      ransmission_Type: "rowValue3",
+    },
   ]);
 };
-
-table.increments("Car_id");
-table.string("Vin_Number").notNullable().unique();
-table.string("Car_Make").notNullable();
-table.string("Car_Model").notNullable();
-table.integer("Mileage").notNullable();
-table.string("Title");
-table.string("Transmission");
