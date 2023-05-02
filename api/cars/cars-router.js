040924b264ed7f92978c942be3affa07139768db
BruvLio <lionel.mbayu@gmail.com>
2023-05-01 19:04:53 -0500

    Connected Server

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,11 @@
 // DO YOUR MAGIC
+const express = require("express");
+const cars = require("./cars-model");
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  console.log("made it to get");
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,15 +1,16 @@
 const express = require("express");
 
+const router = require("./cars/cars-router");
+
 const server = express();
 
 server.use(express.json());
 
-server.use("/api/cars");
+server.use("/api/cars", router);
 
 server.get("/", (req, res) => {
   res.status(200).json({
-    api: "Up and running",
+    api: "up",
   });
 });
-
 module.exports = server;
