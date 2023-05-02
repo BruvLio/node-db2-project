1e07e999f4ed32e218e998d0a024bfe526c18808
BruvLio <lionel.mbayu@gmail.com>
2023-05-01 19:11:14 -0500

    wip

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -5,7 +5,10 @@ const cars = require("./cars-model");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  console.log("made it to get");
+  res.status(200).json({
+    message: "Test",
+  });
+  //   console.log("made it to get");
 });
 
 module.exports = router;
