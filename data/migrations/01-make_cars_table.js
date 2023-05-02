3f162fb5e6c2f3df5fee88087c6ff53bf3bd9bc9
BruvLio <lionel.mbayu@gmail.com>
2023-05-01 20:05:19 -0500

    added first migration

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,11 +1,17 @@
-exports.up = function (knex) {
+exports.up = async function (knex) {
   // DO YOUR MAGIC
-  await knex.schema.createTable('', () => {
-    table.increments('')
-  })
+  await knex.schema.createTable("Cars", (table) => {
+    table.increments("Car_id");
+    table.string("Vin_Number").notNullable().unique();
+    table.string("Car_Make").notNullable();
+    table.string("Car_Model").notNullable();
+    table.integer("Mileage").notNullable();
+    table.string("Title");
+    table.string("Transmission");
+  });
 };
 
-exports.down = function  (knex) {
+exports.down = async function (knex) {
   // DO YOUR MAGIC
-  await knex.schema.dropTableIfExists('')
+  await knex.schema.dropTableIfExists("Cars");
 };
