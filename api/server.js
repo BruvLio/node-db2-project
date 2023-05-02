51b99c128630b16c8271cfd7ddf598cd6004dc02
BruvLio <lionel.mbayu@gmail.com>
2023-05-02 09:23:11 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -8,9 +8,19 @@ server.use(express.json());
 
 server.use("/api/cars", router);
 
-server.get("/", (req, res) => {
-  res.status(200).json({
-    api: "up",
+server.use("*", (req, res, next) => {
+  next({ status: 404, message: "not found" });
+});
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
   });
 });
+
+// server.get("/", (req, res) => {
+//   res.status(200).json({
+//     api: "up",
+//   });
+// });
 module.exports = server;
